2e5efe0fac6b2a2cc0c1a68e780731a2b6f205cb
suresh19942 <61176408+suresh19942@users.noreply.github.com>
2021-02-11 15:15:51 +0530

    Update db.js

--- db.js
+++ db.js
@@ -3,7 +3,7 @@ const mysql = require('mysql');
 const connection = mysql.createConnection({
   host: 'localhost',
   user: 'newuser',
-  password: 'password',
+  password: 'Password@1',
   database: 'user'
 });
 
